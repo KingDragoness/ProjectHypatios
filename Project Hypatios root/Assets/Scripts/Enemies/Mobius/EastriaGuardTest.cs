c8a29ecc02c7cf1c655a84b11d8db4b863298e7d
KingDragoness <king72dragoness@gmail.com>
2022-10-09 11:04:55 +0700

    v.1.2.2 (Prepare nogravity level)

---
+++ Project Hypatios root/Assets/Scripts/Enemies/Mobius/EastriaGuardTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EastriaGuardTest : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed;
+
+    private void Update()
+    {
+        // direction towards target
+        Vector3 relativePos = target.position - transform.position;
+        Quaternion toRotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, speed * Time.deltaTime);
+    }
+
+
+}
