27fcb1e6a8a949ed64be178722f84342d23c6e95
KingDragoness <king72dragoness@gmail.com>
2023-01-07 14:55:22 +0700

    Hypatios 1.4.1 b2 DONE (7 Jan) • Extending chamber: Fort War o Paradox: Cable cars (50 souls), rideable cable car that can quickly moves the player from the bottom to the top/vice-versa. • Extending chamber: No Gravity o New section for randomization (Lab Garden section) – [Enter from hangar vertical leads to halfway laser vertical section] • B-Side music player o The alternative music track (50% chance if not null) o Added B-side music for Theratios (1st stage music)

--- Project Hypatios root/Assets/Scripts/Systems/MusicPlayer.cs
+++ Project Hypatios root/Assets/Scripts/Systems/MusicPlayer.cs
@@ -6,12 +6,21 @@ public class MusicPlayer : MonoBehaviour
 {
 
     public AudioSource musicSource;
+    public AudioClip b_side_Track;
 
     public static MusicPlayer Instance;
 
     private void Awake()
     {
         Instance = this;
+
+        if (b_side_Track != null)
+        {
+            var chance = Random.Range(0f, 1f);
+
+            if (chance > 0.5f)
+                musicSource.clip = b_side_Track;
+        }
     }
 
     private void OnEnable()
