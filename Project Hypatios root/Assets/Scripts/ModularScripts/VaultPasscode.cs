5db5ed41183ab2946021379a557277b351cfc121
KingDragoness <king72dragoness@gmail.com>
2023-03-01 01:11:07 +0700

    Hypatios 1.4.4 b8 (Weapon Vault) DONE (March 1) • Redesign train subway (see at discord test server) • Scriptable object speech queues (SpeechDialogueAsset) • 3d model vault door • Expand Sewers chamber: Weapon Vault o Contain exotic weapons. o Unlock new weapon by completing alternate chambers/WIRED server levels. o Checking trivia “wired.heaven” then Aldrich will interact with the passcode monitor to open it. • Weapon Vault found in the sewers o Lifetaker weapon reward after defeating Ophanim • Test WIRED Network server (Heaven) o WIRED computer network system o The WIRED network can be accessed at any point of the game (as long level 2 Paradox unlocked). o Access Heaven from computer in level 2. o Shield layer not rendered

---
+++ Project Hypatios root/Assets/Scripts/ModularScripts/VaultPasscode.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class VaultPasscode : MonoBehaviour
+{
+
+    public SpeechDialogueAsset dialogue_Success;
+    public SpeechDialogueAsset dialogue_Failed;
+    public AnimatorSetBool doorScript;
+    public Trivia trivia;
+
+    private bool success = false;
+
+    private void OnEnable()
+    {
+        if (success)
+        {
+            doorScript.SetBool(true);
+        }
+    }
+
+    public void TryInteract()
+    {
+        if (success) return;
+
+        if (Hypatios.Game.Check_TriviaCompleted(trivia))
+        {
+            dialogue_Success.TriggerMessage();
+            doorScript.SetBool(true);
+            success = true;
+        }
+        else
+        {
+            dialogue_Failed.TriggerMessage();
+        }
+    }
+
+}
