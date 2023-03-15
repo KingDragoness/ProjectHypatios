eceb7aa295cb2b403c8e4e98fb8ffc043fb10238
KingDragoness <king72dragoness@gmail.com>
2023-03-15 07:51:46 +0700

    Hypatios 1.4.6 b2 (Feedback update, game intro) DONE (March 14) • Game intro ongoing

---
+++ Project Hypatios root/Assets/Scripts/Systems/Opening_Controller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opening_Controller : MonoBehaviour
+{
+    public int framerateTarget = 31;
+
+    private void Start()
+    {
+        Application.targetFrameRate = framerateTarget;
+    }
+}
