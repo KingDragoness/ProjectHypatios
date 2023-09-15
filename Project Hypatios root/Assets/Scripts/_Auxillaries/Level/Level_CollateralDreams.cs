8cb9276e17f30ad46fc70efc469a9dc0215f2c59
KingDragoness <king72dragoness@gmail.com>
2023-09-15 13:29:27 +0700

    Hypatios 1.5.7b14 (Major story boss fight completed, game is now fnially completeable) • Eclipseblazer incremental dialogue • Some dialogue changes and cutscene entrance for Mech fight • Vendrich Eclipse Form o Look at the docs • Hypatios “Endgame” o Intermezzo blackout office o Vendrich Final Form o Fix Vendrich boss fight • We need more music for Vendrich boss fight. • OST for Heavenblazer, Vendrich: o Stage 1: “Phase One/Heavenblazer” o Stage 2 (red): “” (Astronommer not suitable) o Stage 3 (heaven): wired.heaven o Stage 4 (Eclipse): • Vendrich boss fight needs total overhaul… o Remove “dust” phase and replace it with a new scene (Vendrich’s Final Form) • Vendrich Final Form: Located at the WIRED o Extremely fast music, Vendrich is smaller and takes a very distorted form. o Eclipse Phase: Vendrich in armoured super-fast, final form

---
+++ Project Hypatios root/Assets/Scripts/_Auxillaries/Level/Level_CollateralDreams.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Sirenix.OdinInspector;
+
+public class Level_CollateralDreams : MonoBehaviour
+{
+
+    public VideoPlayer videoPlayer;
+
+    private float _time = 0f;
+
+    private void Update()
+    {
+
+        _time += Time.deltaTime;
+    
+        if (_time > 2f && videoPlayer.isPlaying == false)
+        {
+            //Quits the game and delibrately crashes.
+            Application.Quit();
+            Debug.Log("Quit Game");
+        }
+    }
+
+
+
+}
