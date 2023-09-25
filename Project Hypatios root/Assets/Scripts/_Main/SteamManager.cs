defa72724f8a97dd17daa2dc2f9950e15f9d44e4
KingDragoness <king72dragoness@gmail.com>
2023-09-25 20:49:22 +0700

    FAGGOT STEAMWORKS SDK DOESN'T WORK • GODDAMNIT Steammanager got reset script thanks STUPIDGITHUB 100 MB LIMIT o VISUAL STUDIO Unoptimized garbage! 700MB RAM? WHO THE FUCK DESIGN THIS  OPEN VS STUDIO ABSOLUTELY RETARDED CAN CAUSE UNITY TO FUCKING CRASH  INCREASINGLY THINGS STARTED TO USE MORE RAM. PLANNED OBSELENCE IS ENFORCing people to BUY MORE AND NEWER HARDWARE. For FUCK SAKE. o Build game 8 minutes NOW HAVE TO FUCKING WAIT AGAIN FOR ONE STUPID INCREMENTAL CHANGES o Fallback handler could not load library C:/Unity Projects/Builds/Hypatios Project/PROJECT; HYPATIOS_Data/Mono/steam_api64  “FALLBACK Handler could not li-” WHAT YOU IDIOT? GOTS Doesn’t have this problem you retarded cunt. WHAT IS WRONG WITH THIS VERSION o COMMENTED OUT application.quit o NO, THERE IS NO FUCKING ACHIEVEMENT AND I DON’T KNOW HOW TO FUCKING FIX IT BECAUSE THE SDK IS POORLY MAINTAINED and Explained BY IDIOTS. • Sewer level make the Mobius Network PC an unlockable paradox [100 souls].

--- Project Hypatios root/Assets/Scripts/_Main/SteamManager.cs
+++ Project Hypatios root/Assets/Scripts/_Main/SteamManager.cs
@@ -99,14 +99,14 @@ public class SteamManager : MonoBehaviour {
 			if (SteamAPI.RestartAppIfNecessary((AppId_t)2609920)) {
 				Debug.Log("[Steamworks.NET] Shutting down because RestartAppIfNecessary returned true. Steam will restart the application.");
 
-				Application.Quit();
+				//Application.Quit();
 				return;
 			}
 		}
 		catch (System.DllNotFoundException e) { // We catch this exception here, as it will be the first occurrence of it.
 			Debug.LogError("[Steamworks.NET] Could not load [lib]steam_api.dll/so/dylib. It's likely not in the correct location. Refer to the README for more details.\n" + e, this);
 
-			Application.Quit();
+			//Application.Quit();
 			return;
 		}
 
